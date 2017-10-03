3459ac6b4d01db82eaa87c2f800a2384e2e07bd6
Neous1 <Neous1@users.noreply.github.com>
2017-10-02 23:22:23 -0400

    add pseudocode for calculator

---
+++ javascript_calculator/js/app.js
@@ -0,0 +1,27 @@
+// var id = document.getElementById
+
+
+document.getElementById("one").addEventListener("click", displayOnScreen);
+document.getElementById("two").addEventListener("click", displayOnScreen);
+
+
+function displayOnScreen() {
+    // if(document.getElementById("screen").value = ""){
+    //             document.getElementById("screen").value = "1"
+    // }
+
+    // switch (document.getElementById) {
+    //     case "one":
+    //         document.getElementById("screen").value = "1";
+    //         break;
+    //     case two:
+    //         document.getElementById("screen").value = "2";
+    //         break;
+
+    //     default:
+    //         break;
+    // }
+
+    document.getElementById("screen").value = "1"
+    document.getElementById("screen").value = "2"
+}
