90acb5a25d331e4e8438c16bf7492147cc847a84
Neous1 <Neous1@users.noreply.github.com>
2017-10-04 11:06:36 -0400

    declared variables
    push entries to memory in order to remove last entry via pop()
    
    add AC function

--- javascript_calculator/js/app.js
+++ javascript_calculator/js/app.js
@@ -1,40 +1,68 @@
-var memory,num1, num2, operator,result;
+var memory = [],
+    num1=0, num2=0, operator, result=0;
 
-var smallDisplay= document.getElementById("smallDisplay")
+var smallDisplay = document.getElementById("smallDisplay")
 var bigDisplay = document.getElementById("bigDisplay")
 
 
 //display buttons
 var buttons = document.getElementsByClassName("btn");
 // console.log(buttons.length);
-for (var i = 0; i< buttons.length; i++ ){    
+for (var i = 0; i < buttons.length; i++) {
     if(buttons[i].value!=="AC" && buttons[i].value !=="<--" ){
         buttons[i].addEventListener("click", function(){             smallDisplay.value += this.value;
+            memory.push(this.value);
+            // memory+=this.value
+            console.log("Memory", memory);
             console.log("14 .",document.getElementById("smallDisplay").value );
         });        
-    }
-    
+    } 
+
 }
 
 var operators = document.getElementsByClassName("operator");
 // console.log(operators.length)
-for (var j = 0; j<operators.length; j++){
+for (var j = 0; j < operators.length; j++) {
     var val = operators[j].value;
+    //AC
+    if (operators[j].value == "AC") {
+        operators[j].addEventListener("click", function () {
+            smallDisplay.value = "";
+            memory = [];
 
-    if(operators[j].value =="AC"){
-        operators[j].addEventListener("click", function(){
-            smallDisplay.value="";
         });
     };
+    // Backspace
+    if (operators[j].value == "<--" && smallDisplay !== "") {
+        operators[j].addEventListener("click", function () {
+            // smallDisplay.value -= this.value;
+            console.log("mem was ", memory);
+            //  memory=memory.slice(1,-1);
+            memory.pop()
+            console.log("new mem: ", memory)
+            smallDisplay.value=memory.join("");
+        });
+    }
 
+    // //ADDITION ++++++++++++++++++++++++++++++
+    // else if (operators[j].value == "+") {
+    //     operators[j].addEventListener("click", addition());
+    // };
 }
 
-function allClear(){
+function allClear() {
     // console.log("33 . ", document.getElementById("smallDisplay").value)
-   return document.getElementById("smallDisplay").value = "";
-}
-
-
-function addition(){
+    memory = [];
+    document.getElementById("smallDisplay").value = "";
 
 }
+
+
+// function addition() {
+//     // if (num2==0){
+//     //     num1=parseFloat(memory.join(""))
+//     // }
+// memory=memory.toString;
+//     result=eval(memory)
+
+// }
