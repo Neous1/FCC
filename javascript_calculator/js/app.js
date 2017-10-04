3332466b9793e1c98582fa278d1501e4e364fb7c
Neous1 <Neous1@users.noreply.github.com>
2017-10-03 21:26:03 -0400

    revamp index.html adn start working on the app.jsfor the functionality

--- javascript_calculator/js/app.js
+++ javascript_calculator/js/app.js
@@ -1,27 +1,40 @@
-// var id = document.getElementById
+var memory,num1, num2, operator,result;
+
+var smallDisplay= document.getElementById("smallDisplay")
+var bigDisplay = document.getElementById("bigDisplay")
 
 
-document.getElementById("one").addEventListener("click", displayOnScreen);
-document.getElementById("two").addEventListener("click", displayOnScreen);
+//display buttons
+var buttons = document.getElementsByClassName("btn");
+// console.log(buttons.length);
+for (var i = 0; i< buttons.length; i++ ){    
+    if(buttons[i].value!=="AC" && buttons[i].value !=="<--" ){
+        buttons[i].addEventListener("click", function(){             smallDisplay.value += this.value;
+            console.log("14 .",document.getElementById("smallDisplay").value );
+        });        
+    }
+    
+}
+
+var operators = document.getElementsByClassName("operator");
+// console.log(operators.length)
+for (var j = 0; j<operators.length; j++){
+    var val = operators[j].value;
+
+    if(operators[j].value =="AC"){
+        operators[j].addEventListener("click", function(){
+            smallDisplay.value="";
+        });
+    };
+
+}
+
+function allClear(){
+    // console.log("33 . ", document.getElementById("smallDisplay").value)
+   return document.getElementById("smallDisplay").value = "";
+}
 
 
-function displayOnScreen() {
-    // if(document.getElementById("screen").value = ""){
-    //             document.getElementById("screen").value = "1"
-    // }
+function addition(){
 
-    // switch (document.getElementById) {
-    //     case "one":
-    //         document.getElementById("screen").value = "1";
-    //         break;
-    //     case two:
-    //         document.getElementById("screen").value = "2";
-    //         break;
-
-    //     default:
-    //         break;
-    // }
-
-    document.getElementById("screen").value = "1"
-    document.getElementById("screen").value = "2"
 }
